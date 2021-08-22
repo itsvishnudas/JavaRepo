5c9e12eeac57d98462fe7e1f7275324049089f95
itsvishnudas <itsvishnudas@gmail.com>
2021-08-23 03:52:53 +0530

    New zero insertion leet code solution

---
+++ src/javaStudy/ZerosSolution.java
@@ -0,0 +1,22 @@
+package javaStudy;
+
+public class ZerosSolution {
+	
+	public void duplicateZeros(int[] arr) {
+		
+		int a[] = new int[6];
+		
+		int index = 0;
+		
+		for(i=0; i<6; i++) {
+			
+			if(a[i]!=0) {
+				
+			}
+		
+		}
+		
+		
+	}
+
+}
